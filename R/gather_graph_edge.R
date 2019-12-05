2d6656521f1e728080af88fc47d16203b5a248ca
Chun-Hui Gao <gaospecial@gmail.com>
2019-12-05 16:45:12 +0800

    remove edge attribute, add root.

--- R/gather_graph_edge.R
+++ R/gather_graph_edge.R
@@ -2,6 +2,7 @@
 #'
 #' @param df
 #' @param index
+#' @param root
 #'
 #' @return a tibble
 #' @export
@@ -9,30 +10,36 @@
 #' @examples
 #'  ibrary(ccgraph)
 #'  data(OTU)
-#'  nodes <- gather_graph_node(OTU,index = c("p","c","o"))
-#' @import dplyr
-#' @importFrom tidyr unite
-gather_graph_edge <- function(df,index=NULL){
+#'  edges <- gather_graph_edge(OTU,index = c("p","c","o"))
+#' @importFrom tidyr tidyr::unite
+gather_graph_edge <- function(df,index=NULL,root=NULL){
+  require(dplyr)
   if (length(index) < 2){
     stop("please specify at least two index column(s)")
   } else if (length(index)==2){
     data <- df %>% mutate(from=.data[[index[[1]]]]) %>%
-      unite(to,index,sep="/") %>%
+      tidyr::unite(to,index,sep="/") %>%
       select(from,to) %>%
-      mutate_at(c("from","to"),as.character) %>%
-      mutate(edge.level=index[[1]])
+      mutate_at(c("from","to"),as.character)
   } else {
     list <- lapply(seq(2,length(index)), function(i){
       dots <- index[1:i]
-      df %>% unite(from,dots[-length(dots)],sep = "/",remove = F)  %>%
-        unite(to,dots,sep="/") %>%
+      df %>% tidyr::unite(from,dots[-length(dots)],sep = "/",remove = F)  %>%
+        tidyr::unite(to,dots,sep="/") %>%
         select(from,to) %>%
-        mutate_at(c("from","to"),as.character) %>%
-        mutate(edge.level=dots[[length(dots)-1]])
+        mutate_at(c("from","to"),as.character)
     })
     data <- do.call("rbind",list)
   }
   data <- as_tibble(data)
-  data$edge.level <- factor(data$edge.level,levels = index)
-  return(data)
+  if (is.null(root)){
+    return(data)
+  } else {
+    root_data <- df %>% group_by(.dots=index[[1]]) %>%
+      summarise(count=n()) %>%
+      mutate(from=root,to=as.character(.data[[index[[1]]]] )) %>%
+      select(from,to)
+    rbind(root_data,data)
+  }
+
 }
