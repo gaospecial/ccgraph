a119c62db55890f1a796ec147e06fed84c6fe97c
Chun-Hui Gao <gaospecial@gmail.com>
2019-12-05 16:55:09 +0800

    using circle_circle.R to calculate circle_circle layout manually

--- R/circle_circle.R
+++ R/circle_circle.R
@@ -1,3 +1,27 @@
+#' calculate circle_circle layout manually
+#'
+#' @param layout
+#' @param index
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'   libray(tidygraph)
+#'   n <- 1000
+#'   microbiome <- data.frame(
+#'     otu = paste("OTU",1:n,sep="_"),
+#'     phylum = sample(paste("phylum",1:5,sep="_"),n,replace = T),
+#'     class = sample(paste("class",6:30,sep="_"),n,replace=T),
+#'     order = sample(paste("order",31:80,sep="_"),n,replace = T),
+#'     value = runif(n,min=1,max=1000)
+#'   )
+#'   index_micro <- c("phylum","class","order")
+#'   nodes_micro <- gather_graph_node(microbiome,index=index_micro, root="Bac")
+#'   edges_micro <- gather_graph_edge(microbiome,index=index_micro, root="Bac")
+#'   graph_micro <- tbl_graph(nodes_micro,edges_micro)
+#'   layout_micro <- create_layout(graph_micro,layout = "circle")
+#'   layout <- circle_circle(layout_micro,index=index_micro)
 circle_circle <- function(layout,index=NULL){
   if (is.null(index)){
     warning("index is NULL, do nothing")
